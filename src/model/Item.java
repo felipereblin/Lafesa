3d269a6c5095704731b8362dfb5b138e8cee82a7
larissacensi <larissacensi3@gmail.com>
2018-10-30 21:27:35 -0300

    Criação atributos Item
    Co-Authored-By: felipereblin <felipereblin@users.noreply.github.com>
    Co-Authored-By: sabrinadiehl1 <sabrinadiehl1@users.noreply.github.com>

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,5 +1,35 @@
 package model;
 
 public class Item {
-
+	private int id;
+	private Pedido pedido;
+	private Produto produto;
+	private int quantidade;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
 }
